2574e27b20f7394c1ff929cef15fbac4136997e0
alexiaruedagu <alexiaruedagu@gmail.com>
2025-01-27 20:13:03 +0100

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/cv_project/', // Cambia esto al nombre de tu carpeta de despliegue
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? './' : '/cv_project/', 
+  };
 });
